8e1f46a9b437fdcce9de886587f08882ce5d177e
Nitin Golchha <nitin@Nitins-MacBook-Pro.local>
2019-03-09 20:44:19 +0530

    changed arrow key to my liking

--- keyboards/planck/keymaps/njain2686/keymap.c
+++ keyboards/planck/keymaps/njain2686/keymap.c
@@ -51,14 +51,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |SFT/''|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | Del  | GUI  |RAISE |    Space    |LOWER | Left | Down |  Up  |Right |
+ * | Ctrl | Alt  | Del  | GUI  |RAISE |    Space    |LOWER | Left |  UP  | DOWN |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT,KC_QUOT) ,
-    KC_LCTL, KC_LALT, KC_DEL, KC_LGUI, RAISE,   KC_SPC,  KC_SPC,  LOWER,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, KC_LALT, KC_DEL, KC_LGUI, RAISE,   KC_SPC,  KC_SPC,  LOWER,   KC_LEFT, KC_UP, KC_DOWN,   KC_RGHT
 ),
 
 /* Colemak
